ec11fd8b4f10a826dd5395cf5eec0e5ad521876a
Len Rich <lenrich1403@gmail.com>
2024-02-17 15:55:33 +0800

    Disable Server-side Rendering

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchema";
-import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
 // 這邊將zod跟 react-form-hook 還有prisma schema結合在一起
 // 這樣我們前後端就可以使用同一個schema來驗證資料的傳輸了
 type IssueForm = z.infer<typeof createIssueSchema>;
